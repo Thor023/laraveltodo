fdff35e7b5496bc0a13b4ae16ee450df3840d8e2
Thor023 <felipe.castaneda84@gmail.com>
2023-11-13 18:50:31 -0300

    creado formulario basico de registro

--- login-laravel/app/Models/User.php
+++ login-laravel/app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
         'password',
     ];
 
@@ -40,6 +41,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        // 'password' => 'hashed', //linea comentada para efectos de tutorial
     ];
+
+    public function setPasswordAttribute($value){
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
--- login-laravel/routes/web.php
+++ login-laravel/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [RegisterController::class, 'show'] );
+
+
+Route::post('/register', [RegisterController::class, 'register'] );
